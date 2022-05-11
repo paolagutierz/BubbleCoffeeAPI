1976cd42411fb134f59282e5e121da230d438a34
Paola gutierrez <paolagutierzgil@hotmail.com>
2022-05-10 22:10:06 -0500

    modificacion en controlador de pedido

--- src/main/java/io/swagger/api/PedidoApi.java
+++ src/main/java/io/swagger/api/PedidoApi.java
@@ -6,6 +6,7 @@
 package io.swagger.api;
 
 import io.swagger.annotations.*;
+import io.swagger.dto.PagoDTO;
 import io.swagger.dto.PedidoDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -17,79 +18,75 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import javax.validation.Valid;
 import java.util.ArrayList;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2022-05-04T19:53:55.066Z")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2022-05-11T02:16:01.140Z")
 
 @Validated
 @Api(value = "pedido", description = "the pedido API")
-@RequestMapping(value = "/v2")
-
+@RequestMapping(value = "/paolagutierz/BubbleCoffee/2.0.0")
 public interface PedidoApi {
-    @ApiOperation(value = "Generar pedido", nickname = "pedidoPost", notes = "Hacer pedido", response = PedidoDTO.class, tags = {"Pedido",})
-    @ApiResponses(value = {
-            @ApiResponse(code = 201, message = "pedido created", response = PedidoDTO.class),
-            @ApiResponse(code = 400, message = "entrada no valida"),
-            @ApiResponse(code = 409, message = "ya existe el pedido")})
-    @RequestMapping(value = "/pedido",
-            produces = {"application/json"},
-            consumes = {"application/json"},
-            method = RequestMethod.POST)
-    ResponseEntity<PedidoDTO> pedidoPost(@ApiParam(value = "Agregar pedido") @Valid @RequestBody ArrayList<Integer> productos);
 
-
-    @ApiOperation(value = "Cancelar pedido", nickname = "pedidoPedidoIdDelete", notes = "Cancelar pedido", response = PedidoDTO.class, tags = {"Pedido",})
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "pedido cancelado", response = PedidoDTO.class),
-            @ApiResponse(code = 404, message = "no encontrado")})
-    @RequestMapping(value = "/pedido/{pedidoId}",
-            produces = {"application/json"},
-            consumes = {"application/json"},
-            method = RequestMethod.DELETE)
-    ResponseEntity<PedidoDTO> pedidoPedidoIdDelete(@ApiParam(value = "pedido id", required = true) @PathVariable("pedidoId") String pedidoId);
-
-
-    @ApiOperation(value = "Bscar pedido por ID", nickname = "pedidoPedidoIdGet", notes = "Obtener pedido por ID", response = PedidoDTO.class, tags = {"Pedido",})
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "ver pedido", response = PedidoDTO.class),
-            @ApiResponse(code = 400, message = "id incorrecto"),
-            @ApiResponse(code = 404, message = "pedido no encontrado")})
-    @RequestMapping(value = "/pedido/{pedidoId}",
-            produces = {"application/json"},
-            method = RequestMethod.GET)
-    ResponseEntity<PedidoDTO> pedidoPedidoIdGet(@ApiParam(value = "buscar pedido por id", required = true) @PathVariable("pedidoId") String pedidoId);
-
-
-    @ApiOperation(value = "Modificar pedido", nickname = "pedidoPedidoIdPut", notes = "Actualizar pedido", response = PedidoDTO.class, tags = {"Pedido",})
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Pedido actualizado", response = PedidoDTO.class),
-            @ApiResponse(code = 404, message = "Not Found"),
-            @ApiResponse(code = 405, message = "Metodo no permitido")})
-    @RequestMapping(value = "/pedido/{pedidoId}",
-            produces = {"application/json"},
-            consumes = {"application/json"},
-            method = RequestMethod.PUT)
-    ResponseEntity<PedidoDTO> pedidoPedidoIdPut(@ApiParam(value = "cambiar o actualizar pedido por id", required = true) @PathVariable("pedidoId") String pedidoId, @ApiParam(value = "pedido a actualizar") @Valid @RequestBody PedidoDTO pedidoDTO);
-
-
-    @ApiOperation(value = "Obtener detalle del pedido pagado para prepararlo y llevar registro", nickname = "pedidoDetallePedidoIdGet", notes = "detalle de pedido pagado.", response = PedidoDTO.class, tags = {"Pedido",})
+    @ApiOperation(value = "Obtener detalle del pedido pendiente por pagar", nickname = "pedidoDetallePedidoIdGet", notes = "detalle de pedido pagado.", response = PedidoDTO.class, tags = {
+            "Pedido", })
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "pedido encontrado", response = PedidoDTO.class),
             @ApiResponse(code = 400, message = "entrada incorrecta"),
-            @ApiResponse(code = 404, message = "pedido no encontrado")})
-    @RequestMapping(value = "/pedido/detalle/{pedidoId}",
-            produces = {"application/json"},
-            method = RequestMethod.GET)
-    ResponseEntity<PedidoDTO> pedidoDetallePedidoIdGet(@ApiParam(value = "ver detalle de pedido para producirlo", required = true) @PathVariable("pedidoId") String pedidoId);
+            @ApiResponse(code = 404, message = "pedido no encontrado") })
+    @RequestMapping(value = "/pedido/detalle/{pedidoId}", produces = { "application/json" }, method = RequestMethod.GET)
+    ResponseEntity<PedidoDTO> pedidoDetallePedidoIdGet(
+            @ApiParam(value = "ver detalle de pedido para producirlo", required = true) @PathVariable("pedidoId") String pedidoId);
 
-
-    @ApiOperation(value = "Modificar estado del pedido para notificar al mesero o cliente", nickname = "pedidoEstadoPedidoIdPut", notes = "pedido completado", response = PedidoDTO.class, tags = {"Pedido",})
+    @ApiOperation(value = "Pagar pedido", nickname = "PagoPost", notes = "obtener el pedido por Id para pagarlo", response = PedidoDTO.class, tags = {
+            "Pedido", })
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "pedido completado", response = PedidoDTO.class),
+            @ApiResponse(code = 200, message = "Pedido pagado", response = PedidoDTO.class),
             @ApiResponse(code = 400, message = "entrada no valida"),
-            @ApiResponse(code = 404, message = "pedido no encontrado")})
-    @RequestMapping(value = "/pedido/estado/{pedidoId}",
-            produces = {"application/json"},
-            consumes = {"application/json"},
-            method = RequestMethod.PUT)
-    ResponseEntity<PedidoDTO> pedidoEstadoPedidoIdPut(@ApiParam(value = "pedido id", required = true) @PathVariable("pedidoId") String pedidoId);
+            @ApiResponse(code = 409, message = "pago duplicado") })
+    @RequestMapping(value = "/pedido/pago/{pedidoId}", produces = { "application/json" }, consumes = {
+            "application/json" }, method = RequestMethod.POST)
+    ResponseEntity<PagoDTO> PagoPost(
+            @ApiParam(value = "pedido id", required = true) @PathVariable("pedidoId") String pedidoId,
+            @ApiParam(value = "pagar pedido") @Valid @RequestBody PagoDTO pagoDTO);
+
+    @ApiOperation(value = "Cancelar pedido", nickname = "pedidoPedidoIdDelete", notes = "Cancelar pedido", response = PedidoDTO.class, tags = {
+            "Pedido", })
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "pedido cancelado", response = PedidoDTO.class),
+            @ApiResponse(code = 404, message = "no encontrado") })
+    @RequestMapping(value = "/pedido/{pedidoId}", produces = { "application/json" }, consumes = {
+            "application/json" }, method = RequestMethod.DELETE)
+    ResponseEntity<PedidoDTO> pedidoPedidoIdDelete(
+            @ApiParam(value = "pedido id", required = true) @PathVariable("pedidoId") String pedidoId);
+
+    @ApiOperation(value = "Buscar pedido por ID", nickname = "pedidoPedidoIdGet", notes = "Obtener pedido por ID", response = PedidoDTO.class, tags = {
+            "Pedido", })
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "ver pedido", response = PedidoDTO.class),
+            @ApiResponse(code = 400, message = "id incorrecto"),
+            @ApiResponse(code = 404, message = "pedido no encontrado") })
+    @RequestMapping(value = "/pedido/{pedidoId}", produces = { "application/json" }, method = RequestMethod.GET)
+    ResponseEntity<PedidoDTO> pedidoPedidoIdGet(
+            @ApiParam(value = "buscar pedido por id", required = true) @PathVariable("pedidoId") String pedidoId);
+
+    @ApiOperation(value = "Modificar pedido", nickname = "pedidoPedidoIdPut", notes = "Actualizar pedido", response = PedidoDTO.class, tags = {
+            "Pedido", })
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Pedido actualizado", response = PedidoDTO.class),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 405, message = "Metodo no permitido") })
+    @RequestMapping(value = "/pedido/{pedidoId}", produces = { "application/json" }, consumes = {
+            "application/json" }, method = RequestMethod.PUT)
+    ResponseEntity<PedidoDTO> pedidoPedidoIdPut(
+            @ApiParam(value = "cambiar o actualizar pedido por id", required = true) @PathVariable("pedidoId") String pedidoId,
+            @ApiParam(value = "pedido a actualizar") @Valid @RequestBody PedidoDTO pedido);
+
+    @ApiOperation(value = "Generar pedido", nickname = "pedidoPost", notes = "Hacer pedido", response = PedidoDTO.class, tags = {
+            "Pedido", })
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "pedido created", response = PedidoDTO.class),
+            @ApiResponse(code = 400, message = "entrada no valida"),
+            @ApiResponse(code = 409, message = "ya existe el pedido") })
+    @RequestMapping(value = "/pedido", produces = { "application/json" }, consumes = {
+            "application/json" }, method = RequestMethod.POST)
+    ResponseEntity<PedidoDTO> pedidoPost(@ApiParam(value = "Agregar pedido") @Valid @RequestBody ArrayList<Integer> productos);
 
 }
--- src/main/java/io/swagger/api/PedidoApiController.java
+++ src/main/java/io/swagger/api/PedidoApiController.java
@@ -2,7 +2,9 @@ package io.swagger.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.ApiParam;
+import io.swagger.dto.PagoDTO;
 import io.swagger.dto.PedidoDTO;
+import io.swagger.service.PagoService;
 import io.swagger.service.PedidoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,7 +19,7 @@ import javax.validation.Valid;
 import java.io.IOException;
 import java.util.ArrayList;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2022-05-04T19:53:55.066Z")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2022-05-11T02:16:01.140Z")
 
 @Controller
 public class PedidoApiController implements PedidoApi {
@@ -30,18 +32,22 @@ public class PedidoApiController implements PedidoApi {
 
     private PedidoService pedidoService;
 
+    private PagoService pagoService;
+
     @org.springframework.beans.factory.annotation.Autowired
     public PedidoApiController(ObjectMapper objectMapper, HttpServletRequest request, PedidoService pedidoService) {
         this.objectMapper = objectMapper;
         this.request = request;
-        this.pedidoService=pedidoService;
+        this.pedidoService = pedidoService;
     }
 
-    public ResponseEntity<PedidoDTO> pedidoPedidoIdDelete(@ApiParam(value = "pedido id", required = true) @PathVariable("pedidoId") String pedidoId) {
+    public ResponseEntity<PedidoDTO> pedidoDetallePedidoIdGet(
+            @ApiParam(value = "ver detalle de pedido para producirlo", required = true) @PathVariable("pedidoId") String pedidoId) {
         String accept = request.getHeader("Accept");
         if (accept != null && accept.contains("application/json")) {
             try {
-                return new ResponseEntity<PedidoDTO>(objectMapper.readValue("{\"empty\": false}", PedidoDTO.class), HttpStatus.NOT_IMPLEMENTED);
+                return new ResponseEntity<PedidoDTO>(objectMapper.readValue("{\"empty\": false}", PedidoDTO.class),
+                        HttpStatus.NOT_IMPLEMENTED);
             } catch (IOException e) {
                 log.error("Couldn't serialize response for content type application/json", e);
                 return new ResponseEntity<PedidoDTO>(HttpStatus.INTERNAL_SERVER_ERROR);
@@ -51,11 +57,22 @@ public class PedidoApiController implements PedidoApi {
         return new ResponseEntity<PedidoDTO>(HttpStatus.NOT_IMPLEMENTED);
     }
 
-    public ResponseEntity<PedidoDTO> pedidoPedidoIdGet(@ApiParam(value = "buscar pedido por id", required = true) @PathVariable("pedidoId") String pedidoId) {
+    public ResponseEntity<PagoDTO> PagoPost(@ApiParam(value = "pedido id", required = true) @PathVariable("pedidoId") String pedidoId, @ApiParam(value = "pagar pedido") @Valid @RequestBody PagoDTO pagoDTO) {
+        String accept = request.getHeader("Accept");
+        if (accept != null && accept.contains("application/json")) {
+            PagoDTO pago = pagoService.crearPago(pagoDTO);
+            return new ResponseEntity<PagoDTO>(pago, HttpStatus.OK);
+        }
+        return new ResponseEntity<PagoDTO>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    public ResponseEntity<PedidoDTO> pedidoPedidoIdDelete(
+            @ApiParam(value = "pedido id", required = true) @PathVariable("pedidoId") String pedidoId) {
         String accept = request.getHeader("Accept");
         if (accept != null && accept.contains("application/json")) {
             try {
-                return new ResponseEntity<PedidoDTO>(objectMapper.readValue("{\"empty\": false}", PedidoDTO.class), HttpStatus.NOT_IMPLEMENTED);
+                return new ResponseEntity<PedidoDTO>(objectMapper.readValue("{\"empty\": false}", PedidoDTO.class),
+                        HttpStatus.NOT_IMPLEMENTED);
             } catch (IOException e) {
                 log.error("Couldn't serialize response for content type application/json", e);
                 return new ResponseEntity<PedidoDTO>(HttpStatus.INTERNAL_SERVER_ERROR);
@@ -65,11 +82,13 @@ public class PedidoApiController implements PedidoApi {
         return new ResponseEntity<PedidoDTO>(HttpStatus.NOT_IMPLEMENTED);
     }
 
-    public ResponseEntity<PedidoDTO> pedidoPedidoIdPut(@ApiParam(value = "cambiar o actualizar pedido por id", required = true) @PathVariable("pedidoId") String pedidoId, @ApiParam(value = "pedido a actualizar") @Valid @RequestBody PedidoDTO pedidoDTO) {
+    public ResponseEntity<PedidoDTO> pedidoPedidoIdGet(
+            @ApiParam(value = "buscar pedido por id", required = true) @PathVariable("pedidoId") String pedidoId) {
         String accept = request.getHeader("Accept");
         if (accept != null && accept.contains("application/json")) {
             try {
-                return new ResponseEntity<PedidoDTO>(objectMapper.readValue("{\"empty\": false}", PedidoDTO.class), HttpStatus.NOT_IMPLEMENTED);
+                return new ResponseEntity<PedidoDTO>(objectMapper.readValue("{\"empty\": false}", PedidoDTO.class),
+                        HttpStatus.NOT_IMPLEMENTED);
             } catch (IOException e) {
                 log.error("Couldn't serialize response for content type application/json", e);
                 return new ResponseEntity<PedidoDTO>(HttpStatus.INTERNAL_SERVER_ERROR);
@@ -79,43 +98,30 @@ public class PedidoApiController implements PedidoApi {
         return new ResponseEntity<PedidoDTO>(HttpStatus.NOT_IMPLEMENTED);
     }
 
-    public ResponseEntity<PedidoDTO> pedidoPost(@ApiParam(value = "Agregar pedido") @Valid @RequestBody ArrayList<Integer> productos) {
+    public ResponseEntity<PedidoDTO> pedidoPedidoIdPut(
+            @ApiParam(value = "cambiar o actualizar pedido por id", required = true) @PathVariable("pedidoId") String pedidoId,
+            @ApiParam(value = "pedido a actualizar") @Valid @RequestBody PedidoDTO pedido) {
+        String accept = request.getHeader("Accept");
+        if (accept != null && accept.contains("application/json")) {
+            try {
+                return new ResponseEntity<PedidoDTO>(objectMapper.readValue("{\"empty\": false}", PedidoDTO.class),
+                        HttpStatus.NOT_IMPLEMENTED);
+            } catch (IOException e) {
+                log.error("Couldn't serialize response for content type application/json", e);
+                return new ResponseEntity<PedidoDTO>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+
+        return new ResponseEntity<PedidoDTO>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    public ResponseEntity<PedidoDTO> pedidoPost(
+            @ApiParam(value = "Agregar pedido") @Valid @RequestBody  ArrayList<Integer> productos) {
         String accept = request.getHeader("Accept");
         if (accept != null && accept.contains("application/json")) {
             PedidoDTO pedido= pedidoService.crearPedido(productos);
-
             return new ResponseEntity<PedidoDTO>(pedido, HttpStatus.OK);
         }
         return new ResponseEntity<PedidoDTO>(HttpStatus.NOT_IMPLEMENTED);
     }
-
-    public ResponseEntity <PedidoDTO> pedidoDetallePedidoIdGet(@ApiParam(value = "Obtener detalle del pedido pagado para prepararlo y llevar registro", required = true) @PathVariable("pedidoId") String pedidoId) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<PedidoDTO>(objectMapper.readValue("{\"empty\": false}", PedidoDTO.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<PedidoDTO>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
-
-        return new ResponseEntity<PedidoDTO>(HttpStatus.NOT_IMPLEMENTED);
-    }
-
-    public ResponseEntity <PedidoDTO> pedidoEstadoPedidoIdPut(@ApiParam(value = "Modificar estado del pedido para notificar al mesero o cliente", required = true) @PathVariable("pedidoId") String pedidoId) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<PedidoDTO>(objectMapper.readValue("{\"empty\": false}", PedidoDTO.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<PedidoDTO>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
-
-        return new ResponseEntity<PedidoDTO>(HttpStatus.NOT_IMPLEMENTED);
-    }
-
-
 }
